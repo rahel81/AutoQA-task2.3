fb0f96df3ad371409a2f34ab739548c4ae6d00cb
Rahel81 <rahimova81@gmail.com>
2021-03-05 16:17:04 +0300

    fixed PostmanEchoTest v1

--- src/test/java/ru/netology/postman/PostmanEchoTest.java
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("salut"))
+                .body("data", equalTo("salt"))
         ;
     }
 }
